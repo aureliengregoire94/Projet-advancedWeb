afd2a5a8245b37d29311c9689aed276c639542cf
aureliengregoire94 <60916671+tributZ@users.noreply.github.com>
2022-10-20 18:59:20 +0200

    anentissement

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react'
+import Stock from './Stock'
+import './App.css';
+
+function App() {
+  return (
+    <div classname = "App">
+      <Stock />
+    </div>
+  )
+}
+
+export default App;
