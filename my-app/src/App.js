6e35f0f77236c2906be65a66d6550fb4fd2ce0bf
Antoine611 <antoine.baligand@efrei.net>
2022-10-21 01:22:18 +0200

    Création de la page d'accueil

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className = "App">
-      <Stock />
+    <div class="All">
+      <div id="header">
+        <div class="buttons">
+            <button class="button" type="button">
+                <span>Currency</span>
+            </button>
+        </div>
+        <header>Stock and currency analyse platform</header>
+        <div class="buttons">
+            <button class="button" type="button">
+              <span>Stock</span>
+            </button>
+        </div>
+      </div>
+      <div>
+        <form>
+          <div>
+            <h1>Welcome to our website</h1>
+            <p>This website allows you to analyse the value of a stock or the value of a currency</p>
+            <p>Please choose the one you want by cliking on the corresponding button on the top of the page</p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
