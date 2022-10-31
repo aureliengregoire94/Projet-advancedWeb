a0a3bf8346daac6fb845c6256fb2a3ed764e4b55
Thibault PELTIER <thibault.peltier@efrei.net>
2022-10-31 17:02:00 +0100

    fin front

--- my-app/src/Currency.js
+++ my-app/src/Currency.js
@@ -1,7 +1,59 @@
 import React from "react";
-
+import Plot from 'react-plotly.js';
 
 class Currency extends React.Component{
+
+
+    constructor (props) {
+        super(props);
+        
+        this.state = {
+            currencyChartXvalues: [], 
+            currencyChartYvalues: []
+        }
+    }
+
+
+    componentDidMount () {
+        this.fetchcurrency();
+    }
+
+
+    fetchcurrency () {
+
+        const pointerTothis = this
+
+        let currencyChartXvaluesfunction = []
+        let currencyChartYvaluesfunction = []
+
+        const Api_key = "AO8RJ8LVUMAV2H2U"
+        const Symbol = "AMZN"
+        const App_url = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=" + Symbol + "&apikey=" + Api_key
+
+        fetch(App_url)
+        .then(
+            function(response) {
+                return response.json()
+            }
+        )
+        .then(
+            function(data){
+                console.log(data)
+                for(let key in data['Weekly Adjusted Time Series']){
+                    currencyChartXvaluesfunction.push(key)
+                    currencyChartYvaluesfunction.push(data['Weekly Adjusted Time Series'][key]['1. open'])
+                }
+
+                pointerTothis.setState({
+                    currencyChartXvalues: currencyChartXvaluesfunction, 
+                    currencyChartYvalues: currencyChartYvaluesfunction
+                })
+            }
+            
+        )
+
+        console.log(currencyChartYvaluesfunction)
+    }
 
 
     
@@ -9,6 +61,7 @@ class Currency extends React.Component{
     render() {
         return(
             <div>
+                <div class = "currencies">
                 <form id="searchbox1" method="get">
                 <p>Currency 1</p>
                     <br/><br/>
@@ -21,6 +74,22 @@ class Currency extends React.Component{
                  <input name="q" type= "text" size="15" placeholder="Type here ..." />
                  <input id="button-submit" type="submit" value="Search" />
              </form>
+             </div>
+             <form>
+                    <Plot
+                        data={[
+                            {
+                                x: this.state.currencyChartXvalues,
+                                y: this.state.currencyChartYvalues,
+                                type: 'scatter',
+                                mode: 'lines+markers',
+                                marker: {color : 'red'},
+                            }
+
+                        ]}
+                        layout={{width:720, height: 440, title: 'Currencues values'}}
+                    />
+                </form>
             </div>
         )
     }
--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -5,13 +5,17 @@ function Home() {
     return (
         <div>
             <form>
-                <div>
+            
                     <h1>Welcome to our website</h1>
                     <p>This website allows you to analyse the value of a stock or the value of a currency</p>
                     <p>Please choose the one you want by clicking on the corresponding button on the top of the page</p>
-                </div>
+               
             </form>
+            <footer>
+            <h4>Website created by Aurelien Gregoire, Antoine Baligand, Wiktor Wielgos and Thibault Peltier</h4>
+        </footer>
         </div>
+
     );
 }
 
